cf35c0688ea3f23866105b6911d5b08805a89034
jeremy <jeremy_nicolas@outlook.fr>
2024-11-27 10:50:26 +0000

    Modifying task 3

--- code.sql
+++ code.sql
@@ -10,6 +10,19 @@ GROUP BY
 	a.artist_name, c.concert_title
 HAVING COUNT(csl.song_id > 3);
 
+-- 3. Second proposition
+
+CREATE VIEW concerts_summary AS
+SELECT c.concert_title, COUNT(csl.song_id) AS total_songs_performed, SUM(s.length) AS total_time, COUNT(ct.ticket_id) AS total_places_sold
+FROM concerts AS c
+JOIN artist_concerts_link AS acl ON c.concert_id = acl.concert_id
+JOIN concerts_songs_link AS csl ON c.concert_id = csl.concert_id
+JOIN songs AS s ON csl.song_id = s.song_id
+JOIN concerts_tickets AS ct on c.concert_id = ct.concert_id
+GROUP BY
+	c.concert_title
+HAVING COUNT(csl.song_id > 3);
+
 -- 4. Write one BEFORE and one AFTER trigger for the modified schema. 
 
 DELIMITER //
