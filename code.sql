f5d504a0160972c5a8ff4c8ecbdfbb56b6b4148f
jeremy <jeremy_nicolas@outlook.fr>
2024-11-27 10:19:03 +0000

    Grouping all tasks in code.sql

---
+++ code.sql
@@ -0,0 +1,50 @@
+-- 3. Create a view for the modified schema. The view must use a join and both GROUP BY and HAVING clauses. Aim at demonstrating advanced SQL skills.
+
+CREATE VIEW artist_concert_summary AS
+SELECT a.artist_name, c.concert_title, COUNT(csl.song_id) AS total_songs_performed
+FROM artists AS a
+JOIN artist_concerts_link AS acl ON a.artist_id = acl.artist_id
+JOIN concerts AS c ON acl.concert_id = c.concert_id
+JOIN concerts_songs_link AS csl ON c.concert_id = csl.concert_id
+GROUP BY
+	a.artist_name, c.concert_title
+HAVING COUNT(csl.song_id > 3);
+
+-- 4. Write one BEFORE and one AFTER trigger for the modified schema. 
+
+DELIMITER //
+
+CREATE TRIGGER fan_age_check
+BEFORE INSERT ON fans
+FOR EACH ROW BEGIN
+	IF NEW.age < 0 THEN
+    	SIGNAL SQLSTATE '45000' # unhandled user-defined exception
+        SET MESSAGE_TEXT = 'Fans must be at least 18 years old';
+	END IF;
+END;
+
+//
+
+DELIMITER;
+
+
+DELIMITER //
+
+CREATE TRIGGER favorite_artist_update
+AFTER INSERT ON fan_favorites
+FOR EACH ROW BEGIN
+	UPDATE artists
+    SET genre = CONCAT(genre, ' (Fan Favorite)')
+    WHERE artist_id = NEW.artist_id AND NOT FIND_IN_SET('(Fan Favorite)', genre);
+END;
+
+//
+DELIMITER;
+
+-- 5. Write a stored function that returns the total number of occupied seats for a given concert_id.
+
+
+
+-- 6.  Write a stored procedure that checks whether a given song_id is associated with a given album_id. 
+--      If not, insert this association into the database. 
+--      Additionally, adjust the song’s release date if it is later than the album’s.
